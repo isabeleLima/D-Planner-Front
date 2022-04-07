05579df3a09dce0383458f051b5f38b218933bba
João Artur <jartur.dev@gmail.com>
2022-04-07 20:49:55 -0300

    validação de atividade e avaliação

---
+++ js/verifyActivities.js
@@ -0,0 +1,31 @@
+const validate = (inputId, callback, errorMsg) => {
+  const isValid = callback(document.getElementById(inputId)?.value);
+  if (!isValid) throw new Error(errorMsg);
+  return isValid;
+};
+
+const registerActivity = () => {
+  try {
+    validate('name-register', v => v.trim() !== '', 'Insira um nome');
+    validate('subject-register', v => v !== null, 'Selecione uma Cadeira');
+    validate('description-register', v => v.trim() !== '', 'Insira uma descrição');
+    validate('date-register', d => !isNaN(Date.parse(d)), 'Insira uma data válida');
+
+    window.location.reload();
+  } catch (error) {
+    window.alert(error.message);
+  }
+};
+
+const updateActivity = () => {
+  try {
+    validate('name-update', v => v.trim() !== '', 'Insira um nome');
+    validate('subject-update', v => v !== null, 'Selecione uma Cadeira');
+    validate('description-update', v => v.trim() !== '', 'Insira uma descrição');
+    validate('date-update', d => !isNaN(Date.parse(d)), 'Insira uma data válida');
+
+    window.location.reload();
+  } catch (error) {
+    window.alert(error.message);
+  }
+};
